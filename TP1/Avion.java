2c5986d71c3f4043604a5c2a9504745082e6e6bc
Luján Guimpelevich <131202960+marialujan13@users.noreply.github.com>
2024-04-21 09:56:48 -0300

    Update Avion.java
    Modificacion del metodo para retornar un asiento aleatorio del avión, crea una lista auxiliar que va borrando sus elementos a medida que salen asientos

--- TP1/Avion.java
+++ TP1/Avion.java
@@ -1,12 +1,10 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Avion {
 
 // ------------------------------- VARIABLES ---------------------------------------------
-
-    // Definiciones
-    //private static final Integer LIBRE = 0;
-    //private static final Integer RESERVADO = 1;
-    //private static final Integer VERIFICADO = 2;
-    //private static final Integer CANCELADO = -1;
 
     // Tamaño de filas y columnas predefinido
     private Integer filas = 31;
@@ -14,6 +12,8 @@ public class Avion {
 
     // Conjunto de Asientos
     private Asiento asientos[][];
+
+    private List<Asiento> lista_a = new ArrayList<>();
      
 // ------------------------------- FUNCIONES ---------------------------------------------
 
@@ -40,7 +40,7 @@ public class Avion {
     }
 
     // Imprime todos los asientos existentes
-    public void printAsientos(){
+    public synchronized void printAsientos(){
         System.out.println("\n\n");
         for(Integer i = 0; i < filas ; i++){
             for(Integer j = 0; j < columnas; j++){
@@ -52,7 +52,7 @@ public class Avion {
     }
 
     // Imprime en forma de lista, el estado de todos los asientos
-    public void printEstadoAsientos(){
+    public synchronized void printEstadoAsientos(){
         System.out.println("\n\n");
         for(Integer i = 0; i < filas ; i++) for(Integer j = 0; j < columnas; j++){
 
@@ -61,7 +61,7 @@ public class Avion {
     }
 
     // Cambia el estado particular de UN asiento. 
-    public void cambiarEstado(String a,Integer e){
+    public synchronized void cambiarEstado(String a,Integer e){
         for(Integer i = 0; i < filas ; i++) for(Integer j = 0; j < columnas; j++){
             if(asientos[i][j].getAsiento().equals(a)){
                 asientos[i][j].cambiarEstado(e);
@@ -71,19 +71,64 @@ public class Avion {
         System.out.println("El asiento seleccionado no existe.\n");
     }
 
-    //-------------------Agregado para la clase Reservación------------------------------
-        public Asiento[][] getMatriz(){
-            return asientos;
+    //Devuelve el estado de un asiento en particular, en caso de no existir devuelve 100
+    public synchronized Integer getEstadoAsiento(String a){
+        Integer buscado = 100;
+        for(Integer i = 0; i < filas ; i++) for(Integer j = 0; j < columnas; j++){
+            if(asientos[i][j].getAsiento().equals(a)){
+                buscado = asientos[i][j].getEstadoNumerico();
+                break;
+            }
+        }
+        if(buscado == 100){
+            System.out.println("No se encontro el asiento "+a);
+        }
+        return buscado;
+    }
+
+    public Integer getFilas(){  return filas;   }
+    public Integer getColumnas(){   return columnas;  }
+
+    private synchronized void inicializarListaAsientos() {
+        lista_a.clear(); // Vacía la lista antes de volver a llenarla
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                lista_a.add(asientos[i][j]);
+            }
+        }
+    }
+
+    public synchronized Asiento getAsientoAleatorio(){
+        if (lista_a.isEmpty()) {
+            inicializarListaAsientos(); // Inicializa la lista si está vacía
         }
     
-    /*public static void main(String[] args) {
-        Avion a = new Avion();
-        System.out.println("\n\n\n\n\n");
-        a.printAsientos();
-        a.printEstadoAsientos();
-        a.cambiarEstado("30F",VERIFICADO);
-        a.cambiarEstado("30E",5);
-        a.printEstadoAsientos();
+        Random r = new Random();
+        int a = r.nextInt(lista_a.size());
+    
+        Asiento asiento = lista_a.get(a);
+        lista_a.remove(a); // Elimina el asiento de la lista compartida
+        return asiento;
+    }
+
+    public synchronized boolean estaLleno(){
+        boolean lleno = true;
+        for(Integer i = 0; i < filas ; i++) for(Integer j = 0; j < columnas; j++){
+            if(asientos[i][j].getEstadoNumerico() == Asiento.LIBRE){
+                lleno = false;
+                break;
+            }
+        }
+        return lleno;
+    }    
+    // public static void main(String[] args) {
+    //     Avion a = new Avion();
+    //     System.out.println("\n\n\n\n\n");
+    //     a.printAsientos();
+    //     a.printEstadoAsientos();
+    //     a.cambiarEstado("30F",Asiento.OCUPADO);
+    //     a.cambiarEstado("30E",5);
+    //     a.printEstadoAsientos();
   
-    }*/
+    // }
 }
